d5cd0b4b7b8e8d8425c3c0d9b632b6e2426cf32d
liujy118 <308625350@qq.com>
2022-02-01 14:51:20 +0800

    剑指offer 28 对称二叉树

---
+++ src/main/java/Code/JZ28.java
@@ -0,0 +1,33 @@
+package Code;
+
+/**
+ 剑指 Offer 28. 对称的二叉树
+ */
+public class JZ28 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null){
+            return true;
+        }
+        return isSymmetricHelper(root.left,root.right);
+    }
+
+    public boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left) && left.val == right.val;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
