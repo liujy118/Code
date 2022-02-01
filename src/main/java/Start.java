f610a72eabb51f9e5a683fc2c3c06a7649f3ef73
liujy118 <308625350@qq.com>
2022-02-01 22:29:33 +0800

    剑指offer 45

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -5,7 +5,7 @@ public class Start {
     public static void main(String[] args) {
         int o1 = 3;
         int o2 = 30;
-        System.out.println(o1 * (int) Math.pow(10, (int)Math.log10(o2)+1) + o2);
-        System.out.println(o2 * (int) Math.pow(10, (int)Math.log10(o1)+1) + o1);
+        System.out.println(o1 * (int) Math.pow(10, (int) Math.log10(o2) + 1) + o2);
+        System.out.println(o2 * (int) Math.pow(10, (int) Math.log10(o1) + 1) + o1);
     }
 }
