9f1c0cf940c5c07c9a7fd9b95f7aa0f7047d9a36
liujy118 <308625350@qq.com>
2022-02-02 21:29:13 +0800

    剑指offer 55

---
+++ src/main/java/code/JZ55.java
@@ -0,0 +1,27 @@
+package code;
+
+/**
+ * @Author Jingyu Liu
+ * @Description 剑指 Offer 55 - I. 二叉树的深度
+ * @Mark
+ * @Date 2022/2/2 21:23
+ * @EndTime
+ **/
+public class JZ55 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/main/java/code/JZ55_2.java
@@ -0,0 +1,34 @@
+package code;
+
+/**
+ * @Author K
+ * @Description 剑指 Offer 55 - II. 平衡二叉树
+ * @Mark
+ * @Date 2022/2/2 21:26
+ * @EndTime
+ **/
+public class JZ55_2 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
